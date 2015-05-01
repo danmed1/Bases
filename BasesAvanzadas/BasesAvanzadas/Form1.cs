bae7123cda21e6f0631708b98070f8f76297f73b
danmed1 <ldaniel.med@gmail.com>
2015-05-01 12:44:36 -0500

    START AGAIN

---
+++ BasesAvanzadas/BasesAvanzadas/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasesAvanzadas
+{
+    public partial class Form1 : Form
+    {
+        Form2 siguienteForma = new Form2();
+        Form3 formaPaciente = new Form3();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Pacientes(object sender, EventArgs e)
+        {
+            siguienteForma.Show();
+        }
+
+        private void ProfesionalDeLaSalud(object sender, EventArgs e)
+        {
+            formaPaciente.Show();
+        }
+
+        private void Diagnostico(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Salir(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ BasesAvanzadas/BasesAvanzadas/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace BasesAvanzadas
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void altas_Click(object sender, EventArgs e)
+        {
+            Word.Document doc;
+            object index = 1;
+            var archivoWord = new Word.Application();
+            archivoWord.Documents.Add();
+            archivoWord.Visible = true;
+            Object xmlFormat = Word.WdSaveFormat.wdFormatXML;
+            Object f = "prueba1";
+
+            doc = archivoWord.ActiveDocument;
+            
+            doc.SaveAs2(f,xmlFormat);
+            doc.Close();
+        }
+
+        private void guardar() { 
+        }
+
+        private void regresar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
